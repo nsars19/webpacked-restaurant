882ebdfad62d3d7cbb96dae7af45144443275b2c
Nick <nsardonini@gmail.com>
2020-12-11 19:11:11 -0500

    add contactPage function
    creates and adds contact page info to DOM

---
+++ src/modules/contact.js
@@ -0,0 +1,44 @@
+import { Element, appendTo, $ } from './elements'
+
+export const contactPage = () => {
+  const container = $(".main-container")
+  
+  // Contact info
+  // -- telephone 
+  const phoneNumberContainer = new Element({class: 'phoneContainer'}, 'div')
+  const phoneHeader = new Element({class: 'phone-header'}, 'h2')
+  const phoneNumber = new Element({class: 'phone-number'}, 'div')
+
+  phoneHeader.setText("phone")
+  phoneNumber.setText("1-344-123-6789")
+
+  // -- address
+  const addressContainer = new Element({class: 'address-container'}, 'div')
+  const addressHeader = new Element({class: 'address-header'}, 'h2')
+  const address = new Element({class: 'address'}, 'div')
+
+  addressHeader.setText("address")
+  address.setText("285 Boulevard Rd")
+
+  // -- hours
+  const hoursContainer = new Element({class: hours-container}, 'div')
+  const hoursHeader = new Element({class: 'hours-header'}, 'h2')
+  const hours = new Element({class: 'hours'}, 'div')
+
+  hoursHeader.setText("hours")
+  hours.setText("Tues - Thu\n 10a - 11p\n\nFri - Sun\n10a - 1a")
+
+  let layout = [
+    [container, phoneNumberContainer],
+    [phoneNumberContainer, phoneHeader],
+    [phoneNumberContainer, phoneNumber],
+    [container, addressContainer],
+    [addressContainer, addressHeader],
+    [addressContainer, address],
+    [container, hoursContainer],
+    [hoursContainer, hoursHeader],
+    [hoursContainer, hours],
+  ]
+
+  layout.forEach(elem => appendTo(elem[0], elem[1]))
+}

646bececa663fc977934d882e916e4064ae6f89b
Nick <nsardonini@gmail.com>
2020-12-11 19:14:27 -0500

    add contact button event listener skeleton

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,3 +1,12 @@
+import { container } from 'webpack'
 import homePage from './homepage'
+import { contactPage, menuPage } from './pages'
 
 homePage()
+
+const $ = elem => document.querySelector(elem)
+
+// const contact = $('.contact')
+$('.contact').addEventListener('click', () => {
+  
+})

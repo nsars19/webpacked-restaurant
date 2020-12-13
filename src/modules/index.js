eb5f6457845a0356aee8d0f43b41f569c19285c2
Nick <nsardonini@gmail.com>
2020-12-13 15:35:21 -0500

    add event listeners for contact & home menu. add variable
    adds variable for the homePage object. used to access either the default view or the home view.
    adds event listeners for the contact and home tabs.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,23 @@
 import homePage from './homepage'
 import { contactPage, menuPage } from './pages'
 
-homePage()
+const home = homePage()
+home.defaultView()
 
 const $ = elem => document.querySelector(elem)
 
-// const contact = $('.contact')
-$('.contact').addEventListener('click', () => {
-  
+const clearContainer = container => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild)
+  }
+}
+
+$('.home').addEventListener('click', () => {
+  clearContainer($('.main-container'))
+  home.homeView()
 })
+
+$('.contact').addEventListener('click', () => {
+  clearContainer($(".main-container"))
+  contactPage()
+}) 

a5323e1dcad7e0552420d8814fa67e42184e3ddc
Nick <nsardonini@gmail.com>
2020-12-14 14:30:24 -0500

    add & implement data-active attribute flipping functions
    switches data-active value upon switching to different page-tabs, and highlights the tab of
    the currently-viewed page

--- src/modules/index.js
+++ src/modules/index.js
@@ -12,12 +12,38 @@ const clearContainer = container => {
   }
 }
 
+const clearActiveStatus = elem => elem.setAttribute('data-active', false)
+const addActiveStatus = elem => elem.setAttribute('data-active', true)
+
+const menuItems = {
+  home:    $('.home'), 
+  menu:    $('.menu'), 
+  contact: $('.contact')
+}
+
+const clearMenuItemsActiveStatus = () => {
+  for (let item in menuItems) {
+    clearActiveStatus(menuItems[item])
+  }
+}
+
 $('.home').addEventListener('click', () => {
+  clearMenuItemsActiveStatus()
   clearContainer($('.main-container'))
   home.homeView()
+  addActiveStatus(menuItems.home)
 })
 
 $('.contact').addEventListener('click', () => {
+  clearMenuItemsActiveStatus()
   clearContainer($(".main-container"))
   contactPage()
+  addActiveStatus(menuItems.contact)
 }) 
+
+$('.menu').addEventListener('click', () => {
+  clearMenuItemsActiveStatus()
+  clearContainer($('.main-container'))
+  menuPage()
+  addActiveStatus(menuItems.menu)
+})

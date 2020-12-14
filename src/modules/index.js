e93bcfc5245e7f61c3260a2c3e832a1f5bb1074b
Nick <nsardonini@gmail.com>
2020-12-14 14:35:17 -0500

    wrap index.js logic in function

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,49 +1,53 @@
 import homePage from './homepage'
 import { contactPage, menuPage } from './pages'
 
-const home = homePage()
-home.defaultView()
+function main() {
+  const home = homePage()
+  home.defaultView()
 
-const $ = elem => document.querySelector(elem)
+  const $ = elem => document.querySelector(elem)
 
-const clearContainer = container => {
-  while (container.firstChild) {
-    container.removeChild(container.firstChild)
+  const clearContainer = container => {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild)
+    }
   }
-}
 
-const clearActiveStatus = elem => elem.setAttribute('data-active', false)
-const addActiveStatus = elem => elem.setAttribute('data-active', true)
+  const clearActiveStatus = elem => elem.setAttribute('data-active', false)
+  const addActiveStatus = elem => elem.setAttribute('data-active', true)
 
-const menuItems = {
-  home:    $('.home'), 
-  menu:    $('.menu'), 
-  contact: $('.contact')
-}
-
-const clearMenuItemsActiveStatus = () => {
-  for (let item in menuItems) {
-    clearActiveStatus(menuItems[item])
+  const menuItems = {
+    home:    $('.home'), 
+    menu:    $('.menu'), 
+    contact: $('.contact')
   }
+
+  const clearMenuItemsActiveStatus = () => {
+    for (let item in menuItems) {
+      clearActiveStatus(menuItems[item])
+    }
+  }
+
+  $('.home').addEventListener('click', () => {
+    clearMenuItemsActiveStatus()
+    clearContainer($('.main-container'))
+    home.homeView()
+    addActiveStatus(menuItems.home)
+  })
+
+  $('.contact').addEventListener('click', () => {
+    clearMenuItemsActiveStatus()
+    clearContainer($(".main-container"))
+    contactPage()
+    addActiveStatus(menuItems.contact)
+  }) 
+
+  $('.menu').addEventListener('click', () => {
+    clearMenuItemsActiveStatus()
+    clearContainer($('.main-container'))
+    menuPage()
+    addActiveStatus(menuItems.menu)
+  })
 }
 
-$('.home').addEventListener('click', () => {
-  clearMenuItemsActiveStatus()
-  clearContainer($('.main-container'))
-  home.homeView()
-  addActiveStatus(menuItems.home)
-})
-
-$('.contact').addEventListener('click', () => {
-  clearMenuItemsActiveStatus()
-  clearContainer($(".main-container"))
-  contactPage()
-  addActiveStatus(menuItems.contact)
-}) 
-
-$('.menu').addEventListener('click', () => {
-  clearMenuItemsActiveStatus()
-  clearContainer($('.main-container'))
-  menuPage()
-  addActiveStatus(menuItems.menu)
-})
+main()
